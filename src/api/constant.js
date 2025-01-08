ff1fe7d74dc7e6df0ddb448f47cac18646032466
jin <jinohori1111@gmail.com>
2025-01-08 23:22:07 +0900

    2025/01/08 11:20 backendがdeployできたため、Base_Urlを変更

--- src/api/constant.js
+++ src/api/constant.js
@@ -8,7 +8,7 @@ export const API_URLS = {
 }
 
 
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://shopease-backend-caxm.onrender.com';
 
 export const getHeaders = ()=>{
     return {
